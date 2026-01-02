186f5e691a04856d60de164e69773e5d0d94f8ca
itsaymenben <aymen.benamrane@gmail.com>
2026-01-03 00:30:57 +0100

    feat(core): add bach_size as a hyperparameter to the model

--- core/network/network.py
+++ core/network/network.py
@@ -62,25 +62,25 @@ class NeuralNetwork:
         self._is_built = True
 
     def forward(self,
-                input: NDArray) -> List[NDArray]:
+                X: NDArray) -> List[NDArray]:
         if not self._is_built:
             raise RuntimeError("The model must be built before calling 'forward(input)', please call 'build()' first.")
-        outputs = [input]
-        inputs = [input]
+        outputs = [X]
+        inputs = [X]
         for step, layer in enumerate(self._layers):
             inputs.append(np.dot(outputs[-1], self.weights[step]) + self.biases[step])
             outputs.append(layer.activate(inputs[-1]))
         return inputs, outputs # type: ignore
 
     def backward(self,
-                 input: NDArray,
-                 y_true: NDArray,
-                 learning_rate: float) -> float:
-        N_obs = input.shape[0]
+                 X: NDArray,
+                 y: NDArray,
+                 learning_rate: float) -> None:
+        N_obs = X.shape[0]
         gradient_weights = [np.zeros(weight.shape) for weight in self.weights]
         gradient_biases = [np.zeros(bias.shape) for bias in self.biases]
-        inputs, outputs = self.forward(input)
-        derivative_error = self.d_loss(y_true, outputs[-1]) * self._layers[-1].d_activate(inputs[-1])   # Dimension (input_dim * n_neurons of Output Layer)
+        inputs, outputs = self.forward(X)
+        derivative_error = self.d_loss(y, outputs[-1]) * self._layers[-1].d_activate(inputs[-1])   # Dimension (input_dim * n_neurons of Output Layer)
         gradient_biases[-1] = derivative_error.sum(axis=0)  # Dimension (1 * n_neurons of Output Layer)
         gradient_weights[-1] = np.dot(derivative_error.T, outputs[-2]).T / N_obs
 
@@ -92,34 +92,43 @@ class NeuralNetwork:
         for step in range(len(self._layers)):
             self.weights[step] -= learning_rate * gradient_weights[step]
             self.biases[step] -= learning_rate * gradient_biases[step]
-        return self.loss(y_true, outputs[-1])
 
     def fit(self,
-            input: NDArray,
-            y_true: NDArray,
+            X: NDArray,
+            y: NDArray,
             epochs: int,
-            learning_rate: float) -> List[float]:
+            learning_rate: float,
+            batch_size: int = -1) -> List[float]:
+        if (batch_size > X.shape[0] or batch_size <= 0) and batch_size != -1:
+            raise ValueError("'batch_size' should be a positive integer smaller than or equal to the number of input observations, or equal to -1.")
         history = []
-        for epoch in range(1, epochs + 1):   
-            history.append(self.backward(input, y_true, learning_rate))
+        N_obs = X.shape[0]
+        batch_size = batch_size if batch_size != -1 else N_obs
+        for epoch in range(1, epochs + 1):
+            for start in range(0, N_obs, batch_size):
+                end = min(N_obs, start + batch_size)
+                X_batch = X[start:end]
+                y_batch = y[start:end]
+                self.backward(X_batch, y_batch, learning_rate)
+            history.append(self.loss(y, self.predict(X)))
             if epoch % 10 == 0:
-                score = self.score(input, y_true)
+                score = self.score(X, y)
                 logger.info(f"Epoch {epoch} finished with score {score}")
         return history
 
     def predict(self,
-                input: NDArray) -> NDArray:
-        return self.forward(input)[1][-1]
+                X: NDArray) -> NDArray:
+        return self.forward(X)[1][-1]
 
     def score(self,
-              input: NDArray,
-              y_true: NDArray) -> float:
+              X: NDArray,
+              y: NDArray) -> float:
         if not self._is_built:
             raise RuntimeError("The model must be built before calling 'forward(input)', please call 'build()' first.")
-        y_pred = self.predict(input)
-        if y_pred.shape != y_true.shape:
-            raise ValueError(f"Shape mismatch between y_pred {y_pred.shape} and y_true {y_true.shape}.")
+        y_pred = self.predict(X)
+        if y_pred.shape != y.shape:
+            raise ValueError(f"Shape mismatch between y_pred {y_pred.shape} and y_true {y.shape}.")
 
-        return self.loss(y_true, y_pred)
+        return self.loss(y, y_pred)
 
 
